a116d886a05e6ae8f9d46cc6e1eefa6ff2ce9f74
githubanis <anissarker301@gmail.com>
2020-11-04 23:13:46 +0600

    Add project files.

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,35 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCamps()
+        {
+            try
+            {
+                var result = await repository.GetAllCampsAsync();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
